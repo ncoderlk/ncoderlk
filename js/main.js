411b64f6978d4018921f1ea172ef61747e66f671
Sithika Dangampala <105375428+ncoderlk@users.noreply.github.com>
2022-07-14 15:41:13 +0530

    copyright updated

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var years = nyear - 2008;
 var age = years * 12 - 5;
 document.getElementById("age").innerHTML =
   " I'm Currently Spending " + age + " Month of My Life..! ";
-document.getElementById("year").innerHTML = nyear;
+  document.getElementById("year").innerHTML = nyear;
+  document.getElementById("year2").innerHTML = nyear;
     console.error("Project By Ncoderlk __ Sithika Dangampala");
     
     
