b80a68979e4c8b841b45be024bf77edeac30a2f7
Sithika Dangampala <105375428+ncoderlk@users.noreply.github.com>
2022-07-14 13:20:54 +0530

    updated \

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Day
+var nyear = new Date().getFullYear();
+var years = nyear - 2008;
+var age = years * 12 - 5;
+document.getElementById("age").innerHTML =
+  " I'm Currently Spending " + age + " Month of My Life..! ";
+
+console.error("Project By Ncoderlk __ Sithika Dangampala");
