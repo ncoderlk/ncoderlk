49007e8c64799500e3f4b634874e333cacf332b2
Sithika Dangampala <105375428+ncoderlk@users.noreply.github.com>
2022-07-14 13:32:30 +0530

    auto date update added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 //Day
-var nyear = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', () => {
+    
+    var nyear = new Date().getFullYear();
 var years = nyear - 2008;
 var age = years * 12 - 5;
 document.getElementById("age").innerHTML =
   " I'm Currently Spending " + age + " Month of My Life..! ";
 document.getElementById("year").innerHTML = nyear;
-console.error("Project By Ncoderlk __ Sithika Dangampala");
+    console.error("Project By Ncoderlk __ Sithika Dangampala");
+    
+    
+})
